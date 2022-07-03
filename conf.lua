eb2720e7f5ba5216af0954821e070814c0953a30
FredZeroG <fred@fredzerog.com>
2022-07-03 13:52:44 +0200

    Initial commit

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.window.title = "Missiles Encounters v1.0.5 - by FredZeroG"
+    t.identity = "MissilesEncounters" 
+    t.window.width = 960
+    t.window.height = 540
+    t.window.resizable = true
+end
